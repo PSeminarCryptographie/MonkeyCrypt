f1a0b6a7b11a1c357551d8d6056b56a7c07a192f
heptocat <heptocat@gmail.com>
2018-01-23 18:15:04 +0100

    preparing for more options, renamed alphabet to options

---
+++ src/crypt/Options.java
@@ -0,0 +1,62 @@
+package crypt;
+/** 
+ * Diese Klasse SingleTon ist zum Festlegen von den Einstellungen
+ * @author caterina
+ * 
+ *
+ */
+
+
+public class Options{
+	private static Options instance = new Options();
+	private String letters; //uppercase, lowercase, normal, random
+	private String alphabet; 
+	private String exclusion; //none, all, spaces, [user-defined]
+	/**
+	 * make the constructor private so that this class cannot be instantiated
+	 * -> Nur ein Objekt moeglich
+	 */
+
+	private Options(){
+		setAlphabet("abcdefghijklmnopqrstuvwxyz"); 
+	}
+
+	/**
+	 * Get the only object available
+	 * @return instance
+	 */
+	public static Options getInstance(){
+		return instance;
+	}
+	/**
+	 * Zum Zugreifen auf Alphabet
+	 * @return alphabet
+	 */
+	public String getAlphabet() {
+		return alphabet;
+	}
+	/**
+	 * Zum Aendern des Alphabets
+	 * @param myAlphabet (neues Alphabet)
+	 */
+	public void setAlphabet(String myAlphabet) {
+		this.alphabet = myAlphabet;
+	}
+	
+	public void setCase(String c) {
+		letters = c; 
+	}
+	
+	public String getCase() {
+		return letters;
+	}
+	
+	public void setExclusion(String e) {
+		exclusion = e;
+	}
+	
+	public String getExclusion() {
+		return exclusion;
+	}
+
+}
